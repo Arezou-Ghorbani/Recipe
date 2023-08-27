39092e8d2ff71240dbd0059ed967808497d7789a
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-08-27 13:06:07 +0330

    navigation_main.xml added

--- app/src/main/java/com/example/recipe/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipe/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recipe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.recipe.R
 import com.example.recipe.databinding.ActivityMainBinding
 
@@ -9,14 +10,24 @@ class MainActivity : AppCompatActivity() {
     //    binding
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    //    other
+    private lateinit var naveHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setUp NaveHost
+        naveHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return naveHost.navController.navigateUp() || super.onNavigateUp()
     }
 }
