e4fc3a75cfbc10559c8575c5fe4ff9be27323010
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-03 10:28:52 +0330

    ApiServices.kt added

--- app/src/main/java/com/example/recipe/data/network/ApiServices.kt
+++ app/src/main/java/com/example/recipe/data/network/ApiServices.kt
@@ -1,5 +1,15 @@
 package com.example.recipe.data.network
 
+import com.example.recipe.models.register.BodyRegister
+import com.example.recipe.models.register.ResponseRegister
+import com.example.recipe.utils.Constant
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 /**Created by Arezou-Ghorbani on 29,August,2023,Arezoughorbaniii@gmail.com**/
 interface ApiServices {
+    @POST("users/connect")
+    suspend fun postRegister(@Query(Constant.API_KEY) api:String, @Body body: BodyRegister): Response<ResponseRegister>
 }
