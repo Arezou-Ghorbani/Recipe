fad17cb95703636a119b18b392c27c940c81a239
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-03 13:33:03 +0330

    RegisterRepository.kt added

--- app/src/main/java/com/example/recipe/data/repository/RegisterRepository.kt
+++ app/src/main/java/com/example/recipe/data/repository/RegisterRepository.kt
@@ -2,8 +2,12 @@ package com.example.recipe.data.repository
 
 import com.example.recipe.data.source.RemoteDataSource
 import com.example.recipe.models.register.BodyRegister
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+import javax.inject.Singleton
 
 /**Created by Arezou-Ghorbani on 03,September,2023,ArezouGhorbaniii@gmail.com**/
-class RegisterRepository(private val remote: RemoteDataSource) {
+@ActivityRetainedScoped
+class RegisterRepository@Inject constructor(private val remote: RemoteDataSource) {
     suspend fun postRegister(apiKey: String, body: BodyRegister) = remote.postRegister(apiKey, body)
 }
