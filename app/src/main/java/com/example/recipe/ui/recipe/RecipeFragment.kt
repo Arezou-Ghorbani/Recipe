dd419f69317c52f338722cc2772ee965959f4186
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-12 14:08:23 +0330

    item_popular_shimmer.xml and item_recipes_shimmer.xml designed

--- app/src/main/java/com/example/recipe/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/recipe/ui/recipe/RecipeFragment.kt
@@ -1,7 +1,32 @@
 package com.example.recipe.ui.recipe
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipe.databinding.FragmentRecipeBinding
 
 /**Created by Arezou-Ghorbani on 10,September,2023,ArezouGhorbaniii@gmail.com**/
 class RecipeFragment : Fragment() {
+    //    Binding
+    private var _binding: FragmentRecipeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding=  FragmentRecipeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
