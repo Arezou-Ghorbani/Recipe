baf3374fac32ec2aae5dfe9997baf13bea0bb34d
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-12 15:39:53 +0330

    showUserName function implemented

--- app/src/main/java/com/example/recipe/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/recipe/ui/recipe/RecipeFragment.kt
@@ -5,17 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.recipe.databinding.FragmentRecipeBinding
+import com.example.recipe.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**Created by Arezou-Ghorbani on 10,September,2023,ArezouGhorbaniii@gmail.com**/
+@AndroidEntryPoint
 class RecipeFragment : Fragment() {
     //    Binding
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
+
+    //    viewModels
+    private val registerViewModel: RegisterViewModel by viewModels()
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding=  FragmentRecipeBinding.inflate(layoutInflater)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecipeBinding.inflate(layoutInflater)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,10 +33,24 @@ class RecipeFragment : Fragment() {
         binding.apply {
 
         }
+        lifecycleScope.launchWhenStarted {
+            showUserName()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    suspend fun showUserName() {
+        registerViewModel.readData.collect {
+            var userName = it.userName
+            binding.usernameTxt.text = "Hello, $userName ${getEmojiByUnicode()}"
+        }
+    }
+
+    private fun getEmojiByUnicode(): String {
+        return String(Character.toChars(0x1f44b))
     }
 }
