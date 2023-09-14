08b83f18d6cc5351a1b8f3e85c69aee58c71c0a0
Arezou-Ghorbani <ArezouGhorbaniii@gmail.com>
2023-09-14 19:27:39 +0330

    changes in PopularAdapter.kt

---
+++ app/src/main/java/com/example/recipe/ui/adapters/xdgvxdgghf.kt
@@ -0,0 +1,28 @@
+package com.example.recipe.ui.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.recipe.models.recipes.ResponseRecipes
+
+class xdgvxdgghf {
+    private fun setData(items: List<ResponseRecipes.Result>,data: List<ResponseRecipes.Result>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+
+
+    }    private fun setDataa(items: List<ResponseRecipes.Result>,data: List<ResponseRecipes.Result>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+
+
+    }    private fun setDatad(items: List<ResponseRecipes.Result>,data: List<ResponseRecipes.Result>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+
+
+    }    private fun setDataq(items: List<ResponseRecipes.Result>,data: List<ResponseRecipes.Result>) {
+        val adapterDiffUtils = BaseDiffUtils(items, data)
+        val diffUtils = DiffUtil.calculateDiff(adapterDiffUtils)
+
+
+    }
+}
