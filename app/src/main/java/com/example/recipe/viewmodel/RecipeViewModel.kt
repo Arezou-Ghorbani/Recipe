7647f0a2e55e84b4667d42bfb53cd8d58beff372
Arezoughorbgni <arezoughorbgni00@gmail.com>
2023-09-14 16:43:58 +0330

    popular api implemented

--- app/src/main/java/com/example/recipe/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipe/viewmodel/RecipeViewModel.kt
@@ -2,16 +2,33 @@ package com.example.recipe.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.recipe.data.repository.RecipesRepository
 import com.example.recipe.models.recipes.ResponseRecipes
+import com.example.recipe.utils.Constant
 import com.example.recipe.utils.NetworkRequest
+import com.example.recipe.utils.NetworkResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**Created by Arezou-Ghorbani on 13,September,2023,ArezouGhorbaniii@gmail.com**/
 @HiltViewModel
-class RecipeViewModel @Inject constructor(private val repository: RecipesRepository):ViewModel() {
-//    popularApi
-    val popularliveData=MutableLiveData<NetworkRequest<ResponseRecipes>>()
-    fun callPopularApi 271 : 22
+class RecipeViewModel @Inject constructor(private val repository: RecipesRepository) : ViewModel() {
+    //    popularApi
+    fun fillMap(): HashMap<String, String> {
+        val hashMap: HashMap<String, String> = HashMap()
+        hashMap[Constant.API_KEY] = Constant.MY_API_KEY
+        hashMap["sort"]="popularity"
+        hashMap["addRecipeInformation"]="true"
+        return hashMap
+    }
+
+    //    Api
+    val popularliveData = MutableLiveData<NetworkRequest<ResponseRecipes>>()
+    fun callPopularApi() = viewModelScope.launch {
+        popularliveData.value = NetworkRequest.Loading()
+        var response = repository.remote.getRecipes(fillMap())
+        popularliveData.value = NetworkResponse(response).generalNetworkResponse()
+    }
 }
