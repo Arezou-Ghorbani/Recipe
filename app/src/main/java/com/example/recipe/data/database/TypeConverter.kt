b91c38d01fec3b7b19fa752a880505a43df7203c
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-20 12:13:18 +0330

    TypeConverter.kt added

---
+++ app/src/main/java/com/example/recipe/data/database/TypeConverter.kt
@@ -0,0 +1,19 @@
+package com.example.recipe.data.database
+
+import androidx.room.TypeConverter
+import com.example.recipe.models.recipes.ResponseRecipes
+import com.google.gson.Gson
+
+/**Created by Arezou-Ghorbani on 20,September,2023,ArezouGhorbaniii@gmail.com**/
+class TypeConverter {
+    private var gson = Gson()
+
+    @TypeConverter
+    fun recipeToJson(recipes: ResponseRecipes): String {
+        return gson.toJson(recipes)
+    }
+
+    fun jsonToRecipe(json: String): ResponseRecipes {
+        return gson.fromJson(json, ResponseRecipes::class.java)
+    }
+}
