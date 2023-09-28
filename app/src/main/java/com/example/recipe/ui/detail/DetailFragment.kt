94c660e3987ede70532a208de0de0715cfef1194
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-09-28 14:29:56 +0330

    DetailFragment.kt added in navigation_main.xml

--- app/src/main/java/com/example/recipe/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/recipe/ui/detail/DetailFragment.kt
@@ -1,5 +1,9 @@
 package com.example.recipe.ui.detail
 
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /**Created by Arezou-Ghorbani on 27,September,2023,ArezouGhorbaniii@gmail.com**/
-class DetailFragment {
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
 }
